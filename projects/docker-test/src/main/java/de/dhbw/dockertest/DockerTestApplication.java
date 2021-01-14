699928853d6038509a1696cb5fe27b31979f335b
Marc Ströbel <m.stroebel@gmx.de>
2021-01-14 11:00:06 +0100

    spring boot + docker sample

---
+++ projects/docker-test/src/main/java/de/dhbw/dockertest/DockerTestApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTestApplication.class, args);
+	}
+
+}
---
+++ projects/docker-test/src/test/java/de/dhbw/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
