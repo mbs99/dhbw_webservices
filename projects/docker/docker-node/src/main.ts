814eb88a80a061e34d4ed4635fe58bb35810d097
Marc Ströbel <m.stroebel@gmx.de>
2022-01-25 07:54:33 +0100

    bugfix enable shutdown hooks

--- projects/docker/docker-node/src/main.ts
+++ projects/docker/docker-node/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Starts listening for shutdown hooks
+  app.enableShutdownHooks();
+
   await app.listen(3000);
 }
 bootstrap();
