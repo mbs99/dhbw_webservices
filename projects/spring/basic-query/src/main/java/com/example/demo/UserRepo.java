5e894e0f77454dc0b9fb7af2418cfad2b06fd587
Marc Ströbel <m.stroebel@gmx.de>
2023-01-29 14:43:07 +0100

    data.sql

--- projects/spring/basic-query/src/main/java/com/example/demo/UserRepo.java
+++ projects/spring/basic-query/src/main/java/com/example/demo/UserRepo.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     List<User> findByFirstName(String firstName);
+    List<User> findByLastName(String lastName);
+    List<User> findByLastNameAndFirstNameIgnoreCase(String lastName, String firstName);
 }
--- projects/spring/basic-query/src/test/java/com/example/demo/DemoApplicationTests.java
+++ projects/spring/basic-query/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -14,22 +15,14 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertNotNull(userRepo);
 	}
 
 	@Test
 	void queries() {
-		User john = new User();
-		john.setFirstName("John");
-		john.setLastName("Doe");
-
-		User jane = new User();
-		jane.setFirstName("Jane");
-		jane.setLastName("Doe");
-
-    	userRepo.saveAll(Arrays.asList(john, jane));
-
-		userRepo.findByFirstName("Jane").forEach(result -> System.out.println(result));
-
+		assertEquals(1, userRepo.findByFirstName("Jane").size());
+		assertEquals(1, userRepo.findByFirstName("John").size());
+		assertEquals(2, userRepo.findByLastName("Doe").size());
+		assertEquals(1, userRepo.findByLastNameAndFirstNameIgnoreCase("Doe", "john").size());
 	}
-
 }
