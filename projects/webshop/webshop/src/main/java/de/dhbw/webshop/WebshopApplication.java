3191bfd042b4a8acbb8c582a08a82ea45f6c210c
Marc Stroebel <m.stroebel@gmx.de>
2023-01-31 11:43:31 +0100

    refacored

---
+++ projects/webshop/webshop/src/main/java/de/dhbw/webshop/WebshopApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopApplication.class, args);
+	}
+
+}
