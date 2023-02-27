b2b10843a7a86a3c935c92a31f48302f46a4820a
Marc Stroebel <m.stroebel@gmx.de>
2023-02-27 15:21:29 +0100

    refactored

--- projects/error-handling/order-service/src/app.service.ts
+++ projects/error-handling/order-service/src/app.service.ts
@@ -12,6 +12,9 @@ interface Dto {
 @Injectable()
 export class AppService {
 
+  stockUrl = "localhost:3000";
+  paymentUrl = "localhost:3001"
+
   constructor(private readonly httpService: HttpService) {}
 
   async submitOrder(orderDto: OrderDto): Promise<void> {
@@ -29,17 +32,17 @@ export class AppService {
     // proccess only first result
 
     try {
-      response = await firstValueFrom(this.httpService.post('http://localhost:3000/api/products/' + body[0].productId, body[0]));
+      response = await firstValueFrom(this.httpService.post(`http://${this.stockUrl}/api/products/${body[0].productId}`, body[0]));
       console.log(response.data)
 
-      await firstValueFrom(this.httpService.post('http://localhost:3001/api/payment?force-error=true', body[0]));
+      await firstValueFrom(this.httpService.post(`http://${this.paymentUrl}/api/payment?force-error=true`, body[0]));
       console.log(response.data)
     }
     catch(e) {
       console.log(e);
 
       if((e as AxiosError).response.status === 500) {
-        response = await firstValueFrom(this.httpService.delete('http://localhost:3000/api/products/events/' + response));
+        response = await firstValueFrom(this.httpService.delete(`http://${this.stockUrl}/api/products/events/${response}`));
         console.log(response.data)
       } else {
         throw e;
