51a89c0f7288d7d6281699708d3aa72c49800a3f
Marc <m.stroebelgmx.de>
2023-12-21 13:17:16 +0100

    update Spring Boot 3

--- projects/spring/basic-query/src/main/java/com/example/demo/User.java
+++ projects/spring/basic-query/src/main/java/com/example/demo/User.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+// user is a reserved name in H2
+@Table(name = "users")
 public class User {
 
     @Id
--- projects/spring/basic-query/src/test/resources/data.sql
+++ projects/spring/basic-query/src/test/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO user (id,first_name, last_name) VALUES (0,'John', 'Doe');
-INSERT INTO user (id,first_name, last_name) VALUES (1,'Jane', 'Doe');
+INSERT INTO users VALUES (0,'John', 'Doe');
+INSERT INTO users VALUES (1,'Jane', 'Doe');
