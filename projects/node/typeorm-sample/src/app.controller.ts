09fc6c42fa35ec63230d698201d2590a05fd4db8
Marc Ströbel <marc@marc-streobel.de>
2024-12-27 20:35:04 +0100

    delete notes

--- projects/node/typeorm-sample/src/app.controller.ts
+++ projects/node/typeorm-sample/src/app.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
