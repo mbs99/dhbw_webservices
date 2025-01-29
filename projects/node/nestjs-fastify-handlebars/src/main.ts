4b0371bfdf8fb5046494a2690b9f34b96433e45e
Marc Ströbel <m.stroebel@gmx.de>
2025-01-29 21:55:14 +0100

    server side rendering sample

---
+++ projects/node/nestjs-fastify-handlebars/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
